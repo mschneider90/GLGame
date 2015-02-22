b9899e7464caf70b69d152b63369677a3f1bbc13
mschneider90 <michael.schneider90@gmail.com>
2015-02-22 15:50:37 -0800

    Add key enum

---
+++ source/engine/input/keys.hpp
@@ -0,0 +1,53 @@
+#ifndef KEYS_HPP
+#define KEYS_HPP
+
+namespace glEngine
+{
+
+enum class Keys : std::int16_t
+{
+    A = GLFW_KEY_A,
+    B = GLFW_KEY_B,
+    C = GLFW_KEY_C,
+    D = GLFW_KEY_D,
+    E = GLFW_KEY_E,
+    F = GLFW_KEY_F,
+    G = GLFW_KEY_G,
+    H = GLFW_KEY_H,
+    I = GLFW_KEY_I,
+    J = GLFW_KEY_J,
+    K = GLFW_KEY_K,
+    L = GLFW_KEY_L,
+    M = GLFW_KEY_M,
+    N = GLFW_KEY_N,
+    O = GLFW_KEY_O,
+    P = GLFW_KEY_P,
+    Q = GLFW_KEY_Q,
+    R = GLFW_KEY_R,
+    S = GLFW_KEY_S,
+    T = GLFW_KEY_T,
+    U = GLFW_KEY_U,
+    V = GLFW_KEY_V,
+    W = GLFW_KEY_W,
+    X = GLFW_KEY_X,
+    Y = GLFW_KEY_Y,
+    Z = GLFW_KEY_Z,
+    ZERO = GLFW_KEY_0,
+    ONE = GLFW_KEY_1,
+    TWO = GLFW_KEY_2,
+    THREE = GLFW_KEY_3,
+    FOUR = GLFW_KEY_4,
+    FIVE = GLFW_KEY_5,
+    SIX = GLFW_KEY_6,
+    SEVEN = GLFW_KEY_7,
+    EIGHT = GLFW_KEY_8,
+    NINE = GLFW_KEY_9,
+    ESC = GLFW_KEY_ESC,
+    SPACE = GLFW_KEY_SPACE
+};
+
+}
+
+#endif
+
+
