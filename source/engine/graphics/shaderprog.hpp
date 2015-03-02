e60d806071257758ed5c3d6883ef4b299ec9e9d3
mschneider90 <michael.schneider90@gmail.com>
2015-03-01 18:36:31 -0800

    Document ShaderProgram

--- source/engine/graphics/shaderprog.hpp
+++ source/engine/graphics/shaderprog.hpp
@@ -11,12 +11,24 @@ namespace GLEngine
 class VertexShader;
 class FragmentShader;
 
+/*! @brief Represents a shader program, which links a vertex shader and fragment shader
+ */
 class ShaderProgram
 {
 public:
+    /*! @brief Load a ShaderProgram from disk
+     *
+     *  @param vertexShaderPath The path to the vertex shader file
+     *  @param fragmentShaderPath The path to the fragment shader file
+     */
     ShaderProgram(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+    
+    /*! @brief Destroy a ShaderProgram
+     */
     ~ShaderProgram();
     
+    /*! @brief Get the index of the shader program
+     */
     unsigned int getProgramIndex();
 private:
     VertexShader* vs;
