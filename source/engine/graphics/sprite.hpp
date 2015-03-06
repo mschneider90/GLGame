7663d225f456c66a53e00008aebd2733c83d6165
mschneider90 <michael.schneider90@gmail.com>
2015-03-05 20:38:48 -0800

    Add sprite class

---
+++ source/engine/graphics/sprite.hpp
@@ -0,0 +1,17 @@
+#ifndef GLENGINE_SPRITE_HPP
+#define GLENGINE_SPRITE_HPP
+
+namespace GLEngine
+{
+
+class Sprite
+{
+public:
+private:
+    Sprite(const Sprite&) = delete;
+    Sprite& operator=(const Sprite&) = delete;
+}
+
+}
+
+#endif
